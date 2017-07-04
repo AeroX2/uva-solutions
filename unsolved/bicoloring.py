5631872ecc625a25fae6423eb33e66ed68df452b
AeroX2 <james@ridey.email>
2017-07-04 13:36:31 +1000

    Better bicolor code

---
+++ unsolved/bicoloring.py
@@ -0,0 +1,57 @@
+#!/bin/env python
+
+n = int(input())
+while (n > 0):
+
+    nodes = {}
+    for _ in range(int(input())):
+        a,b = map(int, input().split())
+        if a in nodes:
+            nodes[a].append(b)
+        else:
+            nodes[a] = [b]
+
+        if b in nodes:
+            nodes[b].append(a)
+        else:
+            nodes[b] = [a]
+
+    bicolorable = True
+    labels = {}
+
+    if (len(nodes) > 0):
+        first_node = list(nodes.keys())[0]
+        seen = [first_node]
+        stack = [first_node]
+    else:
+        stack = []
+
+    while (len(stack) > 0):
+        node = stack.pop()
+        
+        unique = set()
+        for child in nodes[node]:
+            if not child in seen:
+                seen.append(child)
+                stack.insert(0,child)
+
+            if child in labels:
+                unique.add(labels[child])
+
+        if (len(unique) >= 2):
+            bicolorable = False
+            break
+
+        for i in range(3):
+            if i not in unique:
+                labels[node] = i
+                break
+
+    if (bicolorable):
+        print("BICOLORABLE.")
+    else:
+        print("NOT BICOLORABLE.")
+
+    n = int(input())
+
+
