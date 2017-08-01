03fb09086d2aec5bd3d0fc958a81283dc0830bbf
AeroX2 <james@ridey.email>
2017-08-01 21:19:33 +1000

    C++ version of Airports, Python was too slow :(

--- solved/airports.cpp
+++ solved/airports.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int find(unordered_map<int, int>& parents,
+int find(unordered_map<int,int>& parents,
 		 int vert) {
 	if (parents[vert] != vert) {
 		parents[vert] = find(parents,parents[vert]);
@@ -15,8 +15,8 @@ int find(unordered_map<int, int>& parents,
 	return parents[vert];
 }
 
-void union_set(unordered_map<int, int>& parents,
-			   unordered_map<int, int> ranks,
+void union_set(unordered_map<int,int>& parents,
+			   unordered_map<int,int>& ranks,
 			   int v1,
 			   int v2) {
     int r1 = find(parents, v1);
@@ -32,10 +32,10 @@ void union_set(unordered_map<int, int>& parents,
 	}
 }
 
-void kruskals(unordered_map<int,int>& mst, 
-			  unordered_map<int, int>& parents,
+void kruskals(unordered_map<unsigned int,int>& mst, 
+			  unordered_map<int,int>& parents,
 			  int locations,
-			  vector<tuple<int,int,int>>& edges) {
+			  vector<tuple<int,int,int,int>>& edges) {
 
 	unordered_map<int, int> ranks;
 	for (int i = 1; i < locations+1; i++) {
@@ -44,23 +44,29 @@ void kruskals(unordered_map<int,int>& mst,
 	}
 
 	sort(edges.begin(), edges.end());
-	for (tuple<int,int,int> edge : edges) {
+	for (tuple<int,int,int,int> edge : edges) {
 		unsigned int cost = get<0>(edge);
 		unsigned int parent = get<1>(edge);
 		unsigned int child = get<2>(edge);
 		if (find(parents, parent) != find(parents, child)) {
 			union_set(parents, ranks, parent, child);
-			unsigned int hash = cost;
-			hash *= 37;
-			hash += parent;
-			hash *= 37;
-			hash += child;
-			mst[hash] = cost;
+			//unsigned int hash = cost;
+			//hash *= 37;
+			//hash += parent;
+			//hash *= 37;
+			//hash += child;
+			unsigned int index = get<3>(edge);
+			mst[index] = cost;
 		}
 	}
 }
 
 int main() {
+	//Should probably switch over to scanf
+	//But why is the question time limit so tight
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	int cases;
 	cin >> cases;
 	for (int i = 0; i < cases; i++) {
@@ -69,19 +75,19 @@ int main() {
 		cin >> roads;
 		cin >> airport_cost;
 
-		vector<tuple<int, int, int>> edges;
+		vector<tuple<int,int,int,int>> edges;
 		for (int road = 0; road < roads; road++) {
 			int parent, child, cost;
 			cin >> parent;
 			cin >> child;
 			cin >> cost;
 			if (cost < airport_cost) {
-				edges.push_back(make_tuple(cost,parent,child));
+				edges.push_back(make_tuple(cost,parent,child,road));
+				//edges.push_back(make_tuple(cost,child,parent));
 			}
 		}
 
-		
-		unordered_map<int,int> mst;
+		unordered_map<unsigned int,int> mst;
 		unordered_map<int,int> parents;
 		kruskals(mst, parents, locations, edges);
 
