61d0d3e1d2b89f62d41fcd11081880f9a795423d
AeroX2 <james@ridey.email>
2017-08-05 17:56:36 +1000

    Some solutions

--- solved/frogger.py
+++ solved/frogger.py
@@ -21,21 +21,20 @@ def shortest_path(start, end, nodes):
         cost,node,path = heappop(queue)
 
         if (node == end):
-            print(path)
+            return cost
 
         for other_node in nodes:
             if (other_node == node): continue
 
             #print(node, other_node)
-            cost = dist[node] + distance(node,other_node)
+            cost = max(dist[node], distance(node,other_node))
             if (cost < dist[other_node]):
                 dist[other_node] = cost
                 prev[other_node] = node
                 if (other_node not in queue):
                     heappush(queue,(cost,other_node,path+[other_node]))
 
-    print(prev)
-    return dist
+    return None
          
 def main():
     i = 0
@@ -49,14 +48,13 @@ def main():
         freddy = stones[0]
         fiona = stones[1]
 
-        distance = shortest_path(freddy, fiona, stones)
-        print(distance)
+        min_distance = shortest_path(freddy, fiona, stones)
 
-        distance = filter(lambda x: x != 0, distance.values())
-        min_distance = min(distance)
+        #distance = filter(lambda x: x != 0, distance.values())
+        #min_distance = min(distance)
 
         print('Scenario #%d' % (i+1))
-        print('Frog distance = %.3f' % min_distance)
+        print('Frog Distance = %.3f' % min_distance)
         print()
 
         i += 1
--- solved/largest-prime-divisor.py
+++ solved/largest-prime-divisor.py
@@ -1,15 +1,14 @@
 #!/bin/env python
 
-import math
-import time
-
 #https://stackoverflow.com/questions/19345627/python-prime-numbers-sieve-of-eratosthenes 
 #This version seems to be faster than anything I have tested
 def sieve(n):
     "Return all primes <= n."
     np1 = n + 1
     s = list(range(np1))
+
     s[1] = 0
+    
     sqrtn = int(round(n**0.5))
     for i in range(2, sqrtn+1):
         if s[i]:
@@ -17,15 +16,33 @@ def sieve(n):
     return filter(None, s)
 
 primes = list(sieve(10**7))
-n = int(input())
-while (n > 0):
-    #Filter out primes larger than sqrt(n)
-    for prime in reversed(primes):
-        if (prime != n and n % prime == 0):
-            print(prime)
-            break
-    else:
-        print(-1)
 
-    n = int(input())
+n = abs(int(input()))
+while (n != 0):
+
+    i = 0
+    count = 0
+    max_p = 0
+    while (n > 1):
+        p = primes[i]
+
+        if (p*p > n):
+            break
+
+        if (n%p == 0):
+            count += 1
+            max_p = p
+
+        while (n%p == 0):
+            n //= p
+
+        i += 1
+
+    if (n > 1):
+        count += 1
+        max_p = n
+
+    print(max_p if count > 1 else -1)
+
+    n = abs(int(input()))
 
