6ed560eb3db4d266a3cc1055624a1080b6f543cf
AeroX2 <james@ridey.email>
2017-08-09 02:32:06 +1000

    Blub

--- unsolved/movie-police.py
+++ unsolved/movie-police.py
@@ -1,27 +1,38 @@
 #!/bin/env python
 
+def distance(clip, movie, i, clip_len):
+    movie >>= i
+    mask = (1 << clip_len)-1
+    movie &= mask
+
+    haming_distance = movie ^ clip
+
+    haming_distance = bin(haming_distance).count('1')
+    return haming_distance
+
 def main():
     m,q = map(int, input().split())
 
-    movies = [int(input(),2) for _ in range(m)]
-    clips = [int(input(),2) for _ in range(q)]
+    movies = [input().strip() for _ in range(m)]
+    clips = [input().strip() for _ in range(q)]
 
     for clip in clips:
-
         min_haming = None
         min_index = None
 
-        print("Clip", bin(clip))
+        clip_len = len(clip)
+        clip = int(clip,2)
+
         for i,movie in enumerate(movies):
+            movie_len = len(movie)
+            movie = int(movie,2)
 
-            for ii in range(movie.bit_length()-clip.bit_length()+1):
-                haming_distance = (clip << ii) ^ movie
-                haming_distance &= ~((1 << i) - 1)
-                haming_distance = bin(haming_distance).count('1')
-
+            for ii in range(movie_len-clip_len+1):
+                haming_distance = distance(clip, movie, ii, clip_len)
                 if (min_haming is None or haming_distance < min_haming):
                     min_haming = haming_distance
                     min_index = i+1
+
         print(min_index)
 
 main()
